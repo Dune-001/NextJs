a6c83761a4bde4e0749215d8d39177ed624c65f0
Dune-001 <daggerone24@gmail.com>
2025-10-30 16:08:56 +0300

    finished chapter 12

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
 //export const experimental_ppr = true;
-export cacheComponents = true;
+//export cacheComponents = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
